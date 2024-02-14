bdee88a6ce5282f0d4f1662cbee6697512b04752
Abhiranda <113504478+Abhiranda@users.noreply.github.com>
2024-02-14 22:37:41 +0530

    Add files via upload

---
+++ QuizApp/questions.js
@@ -0,0 +1,58 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Which of the following is a popular programming language for developing mobile apps?",
+    answer: "Swift",
+    options: [
+      "Swift",
+      "COBOL",
+      "Fortran",
+      "Pascal"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CPU stand for?",
+    answer: "Central Processing Unit",
+    options: [
+      "Central Processing Unit",
+      "Central Print Unit",
+      "Computer Personal Unit",
+      "Central Processing Unity"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What is the main function of the firewall in a network?",
+    answer: "To prevent unauthorized access",
+    options: [
+      "To boost the network speed",
+      "To detect and remove viruses",
+      "To prevent unauthorized access",
+      "To manage network traffic"
+    ]
+  },
+    {
+    numb: 4,
+    question: "In computer security, phishing attacks primarily target:",
+    answer: "User information",
+    options: [
+      "Hardware components",
+      "Software vulnerabilities",
+      "Network infrastructure",
+      "User information"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What is cloud computing?",
+    answer: "Storing and accessing data over the internet",
+    options: [
+      "A weather forecasting technology",
+      "Storing and accessing data over the internet",
+      "A method for cooling computer servers",
+      "Computing based on quantum theory"
+    ]
+  }
+];
